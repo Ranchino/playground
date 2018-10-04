f4dec0c2d10621223dc7ccc9c8eb11abe4007296
Ranchino Bahadin <ranjbahadin@live.se>
2018-10-04 11:05:25 +0200

    toggle body color

---
+++ script.js
@@ -0,0 +1,36 @@
+
+function newColor() {
+
+    var style = document.body.style; 
+
+    if (style.backgroundColor == "lightgreen") {
+        style.backgroundColor = "white";
+    } else {
+        style.backgroundColor = "lightgreen";
+    }
+    
+    
+}
+
+ 
+
+
+/*
+var fotballPlayer = {
+    name: "Ranchino Bahadin",
+    age: 28,
+    shirtNumber: 7,
+    position: "striker",
+    gender: "male",
+};
+
+var newFotbollPlayer = fotbollPlayer;
+newFotbollPlayer.name = "Albert Einstien";
+console.log(newFotbollPlayer);
+console.log(fotballPlayer);
+
+var message = "Welcome";
+var newMessage = message;
+newMessage = "Welcome home!";
+console.log(newMessage);
+console.log(message); */
