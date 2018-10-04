b083ece393a1ef40982813a6ad1daf27bfd84743
Ranchino Bahadin <ranjbahadin@live.se>
2018-10-04 11:40:08 +0200

    add randomize bgcolor button

--- script.js
+++ script.js
@@ -12,7 +12,23 @@ function newColor() {
     
 }
 
- 
+function randomColor() {
+    var randomValue = Math.random();
+    if (randomValue < 0.2) {
+        document.body.style.backgroundColor = "blue";
+    } else if (randomValue < 0.4) {
+        document.body.style.backgroundColor = "red";
+    } else if (randomValue < 0.6) {
+        document.body.style.backgroundColor = "black";
+    } else if (randomValue < 0.8) {
+        document.body.style.backgroundColor = "yellow";
+    } else {
+        document.body.style.backgroundColor = "green";
+    }
+    
+}
+
+
 
 
 /*
